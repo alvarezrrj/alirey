ad4452494e98bcfdba5ea72fa118d20dc74a4592
Rodrigo Alvarez <alvarezrrj@gmail.com>
2023-05-09 18:49:24 -0300

    Revert "Replace Vite::asset for relative path"
    This reverts commit d0bcfa12e75a614e00f150e90ddc5492680695ca.

--- resources/views/config/index.blade.php
+++ resources/views/config/index.blade.php
@@ -2,14 +2,14 @@
 <x-app-layout>
 
     @push('styles')
-        <link
+        <link 
             href="{{ Vite::asset('resources/css/config.css') }}"
-            rel="stylesheet"
+            rel="stylesheet" 
             type="text/css"  />
     @endpush
 
     <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Configuration') }}
         </h2>
     </x-slot>
